7e6f64cdf3cb57bad9e6b98338a1375224ee719f
Jim.Loc2 <b3b6b7@gmail.com>
2019-01-25 16:31:34 -0800

    Added function to squich & sort a string

--- chapter13.py
+++ chapter13.py
@@ -2,3 +2,20 @@
 #   Grean Tea Press
 #
 # change for testing branch logic
+
+# name.strip() strips leading and trailing whitespaces out of a string
+
+#Try removing whitespaces, punctuation, and capitalization in a loop
+#     and returning the letters sorted
+def squishsort(string_in) :
+    string_out = ''
+    string_low = string_in.lower()
+    for char in string_low :
+        if char not in string.whitespace :
+            if char not in string.punctuation :
+                string_out += char
+    return ''.join(sorted(string_out))
+
+# Exercise 13.1: Write a program that reads a file, breaks each line into
+#    words, strips whitespace and punctuation from the words, and converts
+#    them to lowercase.
